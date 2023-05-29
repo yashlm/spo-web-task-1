d248ded0441c010d353390435cbf080c0673e026
Yash Chauhan <yashc22@iitk.ac.in>
2023-05-29 21:53:53 +0530

    Task-1 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <section id="container">
+
+        <div class='set1'>
+          <div class='block2'><h1>2</h1></div>
+          <div class='block1'><h1>1</h1></div>
+        </div>
+  
+        <div class='set2'>
+          <div class='block1'><h1>1</h1></div>
+          <div class='block1'><h1>1</h1></div>
+          <div class='block1'><h1>1</h1></div>
+        </div>
+  
+        <div class='set3'>
+          <div class='block3'><h1>3</h1></div>
+        </div>
+  
+      </section>
   );
 }
 
